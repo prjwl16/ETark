97345f6f91346db5ef2f9cb2336c9d755bd4241f
prjwl16 <47629380+prjwl16@users.noreply.github.com>
2021-08-14 15:42:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,5 +27,5 @@ const authRoute = require("./routes/authRoute")
 app.use("/api",authRoute)
 
 //server
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 app.listen(port,()=>{console.log(`Online on ${port}`)})
